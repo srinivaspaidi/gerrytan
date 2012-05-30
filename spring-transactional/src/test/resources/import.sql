fead6197fde432b24a91f23a56978ddbc1161956
gerry.g.tan <gerry.g.tan@gmail.com>
2012-05-30 14:20:13 +0000

    Share project "spring-transactional" into "https://gerrytan.googlecode.com/svn"

---
+++ spring-transactional/src/test/resources/import.sql
@@ -0,0 +1,3 @@
+insert into Account (name, balance, version) values ('foo', 700, 0);
+insert into Account (name, balance, version) values ('cow', 50, 0);
+insert into Account (name, balance, version) values ('moo', 20, 0);
